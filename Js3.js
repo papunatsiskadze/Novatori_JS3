96561f3f36f55e10d41b078d6bff84961ff4ffe3
papunatsiskadze <papunaciskadze@gmail.com>
2023-04-09 21:56:40 +0400

    init commit/Final commit

---
+++ Js3.js
@@ -0,0 +1,150 @@
+// console.log("სალამი,შემოგევლე")
+// let newMessages = true;
+// if(newMessages) {
+//     console.log("შენ გაქვს რამდენიმე წაუკიტხავი მესიჯი")
+// }
+
+
+
+
+// let userBalance = 100;
+// let cartTotal = 200;
+
+// if(userBalance >= cartTotal) {
+//     console.log("გადახდა წარმატებით შესრულდა")
+// }
+// else {
+//     console.log("თქვენ არ გაქვთ საკმარისი თანხა")
+// }
+
+
+
+
+
+// let testScore = 45;
+
+// if(testScore >= 95) {
+//     console.log("გილოცავ,შენ მოხვდი ნოვატორში!")
+// }
+
+// else if(testScore >= 70) {
+//     console.log("გილოცავ,შენ მოხვდი ჰარვარდის უნივერსიტეტში!")
+// }
+// else if(testScore >= 50) {
+//     console.log("გილოცავ შენ გტუ-ში მოხვდი!")
+// }
+// else {
+//     console.log("მომავალ წელს კიდევ სცადეთ :)")
+// }
+
+
+
+
+// let adminSale = 50;
+// let frequentClientSale = 25;
+// let studentSale = 10;
+
+// let userBalance = 30;
+// let cartTotal = 100;
+
+// let userIsAdmin = true;
+// let userIsFrequentClient = true;
+// let userIsStudent = true;
+
+// if(userIsAdmin) {
+//     if(userBalance >= cartTotal - adminSale) {
+//         console.log("გადახდა წარმატებით შესრულდა");
+//     } else {
+//         console.log("თქვენ არ გაქვთ საკმარისი თანხა");
+//     }
+// } else if(userIsFrequentClient) {
+//     if(userBalance >= cartTotal - userIsFrequentClient) {
+//         console.log("გადახდა წარმატებით შესრულდა");
+//     } else {
+//         console.log("თქვენ არ გაქვთ საკმარისი თანხა");
+//     }
+// } else if(userIsStudent) {
+//     if(userBalance >= cartTotal - userIsStudent) {
+//         console.log("გადახდა წარმატებით შესრულდა");
+//     } else {
+//         console.log("თქვენ არ გაქვთ საკმარისი თანხა");
+//     }
+// }
+
+
+
+
+
+// let fuel = 160;
+
+
+// if(fuel >= 80) {
+//     console.log("ბათუმს ქათქათააას :)))")
+// }
+// else if(fuel >= 50) {
+//     console.log("ქუთეისამდეც გვეყოფა მაგ ბენზინი")
+// }
+// else if(fuel >= 30) {
+//     console.log("ხაშურშიც ჩავსულვართ და ეგაა")
+// }
+// else if(fuel >= 20) {
+//     console.log("გორამდეც მივსისინდებით")
+// }
+// else if(fuel >= 10) {
+//     console.log("იგოეთი კაი,მარა მერე რას ვაპირებთ")
+// }
+// else if(fuel >= 160) {
+//     console.log("ბათუმშიც ჩავალთ და უკანაც ჩამოვალთб.გეუბნები რა!")
+// }
+// else if(fuel >= 80 + 70) {
+//     console.log("იგოეთში გეგულება რამე  'კალონკა'")
+// }
+// else if(fuel >= 80 + 60) {
+//     console.log("გორამდეც ჩამოვალთ,ცოტას დავამატებთ და ეგაა")
+// }
+// else if(fuel >= 80 + 50) {
+//     console.log("ხაშურში ხოარ დავრჩენილიავით უკანა გზიდან?")
+// }
+// else if(fuel >= 80 + 10){
+//     console.log("ან ქუთაისში ხო არ გავჩერებულიყავით სანამ თბილისში დავრუნდებოდით")
+// }
+// else{
+//     console.log("იგოეთამდეც ვერ ჩავალთ იცოდე!")
+// }
+
+
+let fuel = 110;
+
+if(fuel >= 160) {
+    console.log("ბათუმშიც ჩავალთ და უკანაც ჩამოვალთб.გეუბნები რა!")
+}
+else if(fuel >= 80 + 70) {
+    console.log("იგოეთში გეგულება რამე  'კალონკა' ?")
+}
+else if(fuel >= 80 + 60) {
+    console.log("გორამდეც ჩამოვალთ,ცოტას დავამატებთ და ეგაა")
+}
+else if(fuel >= 80 + 50) {
+    console.log("ხაშურში ხოარ დავრჩენილიავით უკანა გზიდან?")
+}
+else if(fuel >= 80 + 30){
+    console.log("ან ქუთაისში ხო არ გავჩერებულიყავით სანამ თბილისში დავრუნდებოდით?")
+}
+else if(fuel >= 80) {
+    console.log("ბათუმს ქათქათააას :)))")
+}
+else if(fuel >= 50) {
+    console.log("ქუთეისამდეც გვეყოფა მაგ ბენზინი")
+}
+else if(fuel >= 30) {
+    console.log("ხაშურშიც ჩავსულვართ და ეგაა")
+}
+else if(fuel >= 20) {
+    console.log("გორამდეც მივსისინდებით")
+}
+else if(fuel >= 10) {
+    console.log("იგოეთი კაი,მარა მერე რას ვაპირებთ")
+}
+else{
+    console.log("იგოეთამდეც ვერ ჩავალთ იცოდე")
+}
